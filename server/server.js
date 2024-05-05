74fa467ab34aec7709a4c6ae0eef7cc15b930d1e
Ahmad Bahr <ahmad.bahr@mubs.edu.lb>
2024-05-05 14:59:07 +0300

    Contd.

--- server/server.js
+++ server/server.js
@@ -31,4 +31,54 @@ if (process.env.NODE_ENV === "production") {
     });
   }
 
+  app.use(notFound); // Handle invalid routes
+app.use(errorHandler);
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on PORT ${process.env.PORT}`)
+);
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: "http://localhost:3000",
+  },
+  pingTimeout: 60 * 1000,
+});
+
+io.on("connection", (socket) => {
+  console.log("Connected to socket.io");
+
+  socket.on("setup", (userData) => {
+    socket.join(userData._id);
+    socket.emit("connected");
+  });
+
+  socket.on("join chat", (room) => {
+    socket.join(room);
+    console.log("User joined room " + room);
+  });
+
+  socket.on("typing", (room) => socket.in(room).emit("typing"));
+
+  socket.on("stop typing", (room) => socket.in(room).emit("stop typing"));
+
+  socket.on("new message", (newMessageRecieved) => {
+    let chat = newMessageRecieved.chat[0]; // Change it to object
+
+    if (!chat.users) return console.log("chat.users not defined");
+
+    chat.users.forEach((user) => {
+      if (user._id === newMessageRecieved.sender._id) return;
+
+      socket.in(user._id).emit("message recieved", newMessageRecieved);
+    });
+  });
+
+  socket.off("setup", () => {
+    console.log("User Disconnected");
+    socket.leave(userData._id);
+  });
+});
+
+
 
